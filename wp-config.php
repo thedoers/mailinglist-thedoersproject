7fa3a7be51e04451795a0b9c282f9a682b8c072d
Piermaria Cosina <piermaria@thedoersrpoject.com>
2013-05-25 00:14:10 +0200

    multisite

--- wp-config.php
+++ wp-config.php
@@ -71,6 +71,14 @@ define('WPLANG', '');
  */
 define('WP_DEBUG', false);
 
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', false );
+$base = '/';
+define( 'DOMAIN_CURRENT_SITE', 'thedoersproject.herokuapp.com' );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
