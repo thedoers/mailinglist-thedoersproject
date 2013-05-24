130949a4abef8a500dad3a643590a4b10541d100
Piermaria Cosina <piermaria@thedoersrpoject.com>
2013-05-25 00:34:50 +0200

    try fix

--- wp-config.php
+++ wp-config.php
@@ -70,14 +70,8 @@ define('WPLANG', '');
  * in their development environments.
  */
 define('WP_DEBUG', false);
+define('WP_ALLOW_MULTISITE', true);
 
-define( 'MULTISITE', true );
-define( 'SUBDOMAIN_INSTALL', false );
-$base = '/';
-define( 'DOMAIN_CURRENT_SITE', 'thedoersproject.herokuapp.com' );
-define( 'PATH_CURRENT_SITE', '/' );
-define( 'SITE_ID_CURRENT_SITE', 1 );
-define( 'BLOG_ID_CURRENT_SITE', 1 );
 
 /* That's all, stop editing! Happy blogging. */
 
