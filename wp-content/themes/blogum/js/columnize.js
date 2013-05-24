0b4d771d157cef3efbe640a0e461e49ad0b515dd
Piermaria Cosina <piermaria@thedoersrpoject.com>
2013-05-24 13:22:07 +0200

    initial commit

---
+++ wp-content/themes/blogum/js/columnize.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($){
+    
+        $('.mcol').makeacolumnlists({cols: 3, colWidth: 305, equalHeight: 'ul', startN: 1});
+        $('.mcol2').makeacolumnlists({cols: 3, colWidth: 305, equalHeight: 'ul', startN: 1});
+    });
