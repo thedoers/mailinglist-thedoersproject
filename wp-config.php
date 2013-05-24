b6caec41ed4f97b985ff000c5f6a6529f0eaf9b7
Piermaria Cosina <piermaria@thedoersrpoject.com>
2013-05-24 14:50:35 +0200

    config for cleardb database

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_test');
+define('DB_NAME', 'heroku_c6558b61963fc96');
 
 /** MySQL database username */
-define('DB_USER', 'tester');
+define('DB_USER', 'b42d2bd73e001a');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'tester');
+define('DB_PASSWORD', '21c55146');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'eu-cdbr-west-01.cleardb.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
