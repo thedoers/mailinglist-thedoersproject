17d6f7fac333e9c2c75ebdefc247c538ebb7c0db
Piermaria Cosina <piermaria@thedoersrpoject.com>
2013-05-25 00:40:19 +0200

    new htaccess

--- wp-config.php
+++ wp-config.php
@@ -70,7 +70,12 @@ define('WPLANG', '');
  * in their development environments.
  */
 define('WP_DEBUG', false);
-define('WP_ALLOW_MULTISITE', true);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'thedoersproject.herokuapp.com');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 
 /* That's all, stop editing! Happy blogging. */
